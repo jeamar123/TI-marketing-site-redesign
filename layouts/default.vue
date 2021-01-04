c07811516bfc288a9761a1b073b35645165e6b95
anastasiia.shlapachenko <anastasia1melnyk@gmail.com>
2021-01-04 11:30:19 +0000

    prepared project

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,24 +3,3 @@
     <Nuxt />
   </div>
 </template>
-
-<style>
-html {
-  font-family: 'Open Sans', sans-serif;
-  font-size: 18px;
-  line-height: 2;
-  font-weight: normal;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const development = process.env.NODE_ENV !== 'production';
+
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -15,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~/assets/scss/index.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -26,20 +28,31 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
+    // '@nuxtjs/stylelint-module',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/svg-sprite',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    baseURL: development
+      ? 'https://test.exploitcon.com'
+      : 'https://exploitcon.com',
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    postcss: {
+      plugins: {
+        'postcss-normalize': {},
+      },
+    },
+  },
 }
