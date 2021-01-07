ab52e6ca63483a7b95430eeb3c7a07fe95722579
anastasiia.shlapachenko <anastasia1melnyk@gmail.com>
2021-01-07 14:01:22 +0000

    add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <Nuxt />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from '~/components/features/Footer';
+
+export default {
+  name: 'Layout',
+  components: {
+    Footer,
+  },
+}
+</script>
