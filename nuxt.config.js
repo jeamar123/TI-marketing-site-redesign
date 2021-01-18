79b1b4d1a0815a14dbd8e617af69adb30ca0deed
anastasiia.shlapachenko <anastasia1melnyk@gmail.com>
2021-01-18 17:08:06 +0000

    add upcoming event api

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,8 +45,8 @@ export default {
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
     baseURL: development
-      ? 'https://test.exploitcon.com'
-      : 'https://exploitcon.com',
+      ? 'https://dev.exploitcon.com/api'
+      : 'https://exploitcon.com/api',
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
