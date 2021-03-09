766c9aea3a749970b79fd20b4b27f90611c0279b
Anastasiia Shlapachenko <anastasia1melnyk@gmail.com>
2021-03-09 15:03:15 +0200

    add cookie policy page

--- components/policies/PolicyHeader.vue
+++ components/policies/PolicyHeader.vue
@@ -1,5 +1,8 @@
 <template>
-  <header class="policy-header">
+  <header
+    class="policy-header"
+    :class="{'policy-header--flat': !headingFollower}"
+  >
     <Heading
       type="h1"
       color="white"
@@ -48,6 +51,10 @@ export default {
   background-color: $black;
   padding: 77px 16px 37px 16px;
 
+  &--flat {
+    padding-bottom: 77px;
+  }
+
   &__heading-follower {
     margin-top: 8px;
     text-align: center;
@@ -55,22 +62,42 @@ export default {
 
   @media (min-width: $media-xs) {
     padding: 77px 16px 47px 16px;
+
+    &--flat {
+      padding-bottom: 77px;
+    }
   }
 
   @media (min-width: $media-sm) {
     padding: 168px 12% 64px 12%;
+
+    &--flat {
+      padding-bottom: 83px;
+    }
   }
 
   @media (min-width: $media-md) {
     padding: 178px 12% 66px 12%;
+
+    &--flat {
+      padding-bottom: 146px;
+    }
   }
 
   @media (min-width: $media-lg) {
     padding: 178px 20% 58px 20%;
+
+    &--flat {
+      padding-bottom: 106px;
+    }
   }
 
   @media (min-width: $media-xl) {
     padding: 178px 25% 58px 25%;
+
+    &--flat {
+      padding-bottom: 106px;
+    }
   }
 }
 </style>
--- pages/cookie-policy.vue
+++ pages/cookie-policy.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="">
-    cookie policy page
-  </div>
+  <PolicyPage
+    :data="data"
+    heading="Cookie policy"
+  />
 </template>
 
 <script>
+import data from '~/static/policies/cookie-policy';
+import PolicyPage from '~/components/policies/PolicyPage';
+
 export default {
   name: 'CookiePolicyPage',
   props: {},
-  components: {},
-  data: () => ({}),
+  components: {
+    PolicyPage,
+  },
+  data: () => ({
+    data,
+  }),
   computed: {},
   methods: {},
 };
